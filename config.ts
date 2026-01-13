ce019a655e2084e264a55eb24d9d5020a33ca7c3
tugagilportugal-star <tugagilportugal@gmail.com>
2026-01-13 17:50:56 +0000

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,18 @@
 export const ASSETS = {
   HERO_BG: "https://i.postimg.cc/xCHQMjQ4/Tugagil-608.jpg",
   LISBON_BG: "https://i.postimg.cc/wjFytpcV/Lisboa.jpg",
+  // Logos Antigos (Mantidos para compatibilidade)
   SPONSOR_LOGO: "https://i.postimg.cc/W4kCf9HB/Logo-Horizontal-Full-Color-Scrum-Alliance.png",
+  // Logo TugÁgil
   TUGAGIL_LOGO: "https://i.postimg.cc/BZ1vPQL9/logo-Branca.png",
-  // Novo Logo RSG Lisbon 2026 (Quadrado Branco para o menu da esquerda)
-  RSG_LOGO_2026: "https://i.postimg.cc/wMpSz32V/2026-RSG-Logo-Lisbon-Reversed.png", 
+  // --- NOVOS LOGOS ---
+  SCRUM_ALLIANCE_LOGO: "https://i.postimg.cc/W4kCf9HB/Logo-Horizontal-Full-Color-Scrum-Alliance.png",
+  // E esta também:
+  RSG_LOGO_2026: "https://i.postimg.cc/W4kCf9HB/Logo-Horizontal-Full-Color-Scrum-Alliance.png", 
 
   RECAP_VIDEO: "https://youtu.be/DK0RsE584S4",
   RECAP_VIDEO_POSTER: "https://img.youtube.com/vi/DK0RsE584S4/maxresdefault.jpg",
+  
   GALLERY_IMAGES: [
     "https://i.postimg.cc/CxX1nH6r/Tugagil-18.jpg",
     "https://i.postimg.cc/Y2RCzwsz/Tugagil-32.jpg",
@@ -19,6 +24,7 @@ export const ASSETS = {
     "https://i.postimg.cc/qvcwJTXW/Tugagil-610.jpg",
     "https://i.postimg.cc/br20Kcwk/Tugagil-618.jpg",
   ],
+  
   // Services config
   SERVICES: {
     SUPABASE_URL: (import.meta.env?.VITE_SUPABASE_URL as string) || "",
