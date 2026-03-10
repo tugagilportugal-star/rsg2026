098936764a6d249d53349ff98684671488dc5d82
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-10 13:13:49 +0000

    Update TicketPurchaseModal.tsx

--- components/TicketPurchaseModal.tsx
+++ components/TicketPurchaseModal.tsx
@@ -254,6 +254,7 @@ export const TicketPurchaseModal: React.FC = () => {
             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-brand-blue focus:border-brand-blue"
           />
         </div>
+      </div>
 
       <div>
         <label className="block text-sm font-medium text-gray-700">Função</label>
