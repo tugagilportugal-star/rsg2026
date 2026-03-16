e27920b1af4395265abe7254697ea6e9057b2cc7
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-16 16:39:23 +0000

    Update Speakers.tsx

--- sections/Speakers.tsx
+++ sections/Speakers.tsx
@@ -206,7 +206,7 @@ export const Speakers: React.FC = () => {
         <div className="mb-24">
             <h3 className="text-2xl font-black text-brand-darkBlue mb-10 text-center md:text-left flex items-center justify-center md:justify-start gap-3">
                 <Sparkles className="text-brand-orange w-6 h-6" />
-                Featured Keynotes
+                Keynotes
             </h3>
             {/* Grelha com justify-items-center para manter os cartões centrados mesmo que haja espaço */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 justify-items-center">
