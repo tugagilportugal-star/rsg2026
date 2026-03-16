5f4b66b9390cff6b2c43c8b474a4bd196c62ebda
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-16 14:38:43 +0000

    Update Speakers.tsx

--- sections/Speakers.tsx
+++ sections/Speakers.tsx
@@ -210,7 +210,7 @@ export const Speakers: React.FC = () => {
                 Keynotes
             </h3>
             {/* Adapta-se perfeitamente: 1, 2 ou 3 colunas dependendo do ecrã */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {keynotes.map(keynote => (
                     <KeynoteCard key={keynote.id} data={keynote} />
                 ))}
