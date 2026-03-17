7ec343de6a445719db925ffc134a00f654298169
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-17 10:20:27 +0000

    Update Speakers.tsx

--- sections/Speakers.tsx
+++ sections/Speakers.tsx
@@ -86,7 +86,6 @@ export const Speakers: React.FC = () => {
       id: `tba-${i+5}`,
       name: "Revealing Soon",
       role: "Agile Practitioner",
-      company: "TBA",
       image: "",
       linkedin: "",
       isRevealed: false
@@ -219,7 +218,7 @@ export const Speakers: React.FC = () => {
         {/* 2. Secção de Speakers */}
         <div>
             <h3 className="text-2xl font-black text-brand-darkBlue mb-10 text-center md:text-left">
-                Speakers & Panelists
+                Speakers
             </h3>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-6 gap-y-12">
                 {speakers.map(speaker => (
