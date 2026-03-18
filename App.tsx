f4990c1ff949b65186b455c0c070c89d3305f187
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-18 10:22:40 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -56,6 +56,7 @@ const App: React.FC = () => {
       <FAQ />
       <Team />
       <Footer />
+    </div>
 
       {/* Botão Admin Escondido */}
       <div className="fixed bottom-6 right-6 z-50">
