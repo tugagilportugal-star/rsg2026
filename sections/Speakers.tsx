cebebe1f3424720f82b3b77d6a65f378164e0f65
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-21 23:32:20 +0000

    Update Speakers.tsx

--- sections/Speakers.tsx
+++ sections/Speakers.tsx
@@ -34,14 +34,6 @@ export const Speakers: React.FC = () => {
       linkedin: "",
       isRevealed: false
     },
-    {
-      id: "k3",
-      name: "TBA",
-      role: "Industry Expert",
-      image: "",
-      linkedin: "",
-      isRevealed: false
-    }
   ];
 
   // ==========================================
@@ -77,9 +69,17 @@ export const Speakers: React.FC = () => {
       id: "s4",
       name: "Paulo Caroli",
       role: "Author of Lean Inception & Team OKRs",
-      image: "https://ui-avatars.com/api/?name=Paulo+Caroli&background=003F59&color=fff&size=400",
+      image: "https://i.postimg.cc/NjDKxV5z/Paulo-Caroli.png",
       linkedin: "https://www.linkedin.com/in/paulocaroli/",
-      isRevealed: false // Mude para true quando anunciar
+      isRevealed: true
+    },
+    {
+      id: "s5",
+      name: "Paulo Caroli",
+      role: "Author of Lean Inception & Team OKRs",
+      image: "https://i.postimg.cc/NjDKxV5z/Paulo-Caroli.png",
+      linkedin: "https://www.linkedin.com/in/paulocaroli/",
+      isRevealed: false
     },
     // Placeholders para manter a grelha de 10 speakers planeados
     ...Array.from({ length: 6 }).map((_, i) => ({
