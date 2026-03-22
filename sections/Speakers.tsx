f40a0e040e8860d200a0b76a9f39a4b83f30d499
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-22 14:43:46 +0000

    Update Speakers.tsx

--- sections/Speakers.tsx
+++ sections/Speakers.tsx
@@ -200,7 +200,7 @@ export const Speakers: React.FC = () => {
                 Keynotes
             </h3>
             {/* Grelha com justify-items-center para manter os cartões centrados mesmo que haja espaço */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 justify-items-center">
                 {keynotes.map(keynote => (
                     <KeynoteCard key={keynote.id} data={keynote} />
                 ))}
