7f6b39a0e1952a9fd9eec2ea831c11dac21e7eee
tugagilportugal-star <tugagilportugal@gmail.com>
2026-03-22 00:13:13 +0000

    Update Hero.tsx

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -39,7 +39,7 @@ export const Hero: React.FC = () => {
         </div>
 
         <div className="text-2xl md:text-4xl lg:text-5xl text-gray-200 font-light uppercase tracking-[0.4em] md:tracking-[0.6em] mb-8 drop-shadow-lg pl-2">
-          LISBOA 2026
+          LISBON 2026
         </div>
 
         <p className="text-lg md:text-xl text-gray-300 mb-12 max-w-2xl mx-auto font-medium drop-shadow-md">
